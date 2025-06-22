46d262f006e5a7f33484be53d26014158f57e5ff
Joseph Mwangi <korimwangi01@gmail.com>
2025-06-22 12:26:55 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const API_URL = 'http://localhost:3001/posts';
+const postList = document.getElementById('post-list');
+        const postDetail = document.getElementById('post-detail');
+        const newPostForm = document.getElementById('new-post-form');
+        const editPostForm = document.getElementById('edit-post-form');
+        const cancelAddBtn = document.getElementById('cancel-add');
+        const cancelEditBtn = document.getElementById('cancel-edit');
+        const newPostFormContainer = document.getElementById('new-post-form-container');
+        const editPostFormContainer = document.getElementById('edit-post-form-container');
+        const postCountElement = document.getElementById('post-count');
+        
